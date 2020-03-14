2775dd015613a42bec8ab52c5191134272334198
MaxPrehoda <maxprehoda@gmail.com>
2020-03-13 19:52:35 -0700

    Broken, still working on Urwid menu UI

--- main.py
+++ main.py
@@ -6,84 +6,79 @@ import entry_db
 import journal_entry
 from edit import EditDisplay
 from io import StringIO
-from menu import Choice
 import urwid
 
-class MenuHandler():
 
-    def item_chosen(self):
-        if Choice.caption 
+def greeting(self,db):
+    entries=db.get_all_entries()
+    if entries:
+        time_humanized=entries[0].creation_date().humanize()
+        current=arrow.now()
+        if current.hour < 12:
+            date = 'morning'
+        elif current.hour > 12:
+            date = 'afternoon'
+        elif current.hour > 6:
+            date = 'evening'
+        return f"Good {date}, you last made an entry {time_humanized}"
 
-    def greeting(self,db):
-        entries=db.get_all_entries()
-        if entries:
-            time_humanized=entries[0].creation_date().humanize()
-            current=arrow.now()
-            if current.hour < 12:
-                date = 'morning'
-            elif current.hour > 12:
-                date = 'afternoon'
-            elif current.hour > 6:
-                date = 'evening'
-            return f"Good {date}, you last made an entry {time_humanized}"
+def menu(self):
+    db = entry_db.EntryDB("entries.db")
+    print(greeting(db))
+    while True:
+        print("1. Make Entry\n2. Browse\n3. Quit")
+        select = input()
+        if select == "1":
+            input_text = multi_input("How are you feeling today?")
+            entry = journal_entry.JournalEntry(input_text)
+            db.add_entry(entry)
+            db.flush_to_disk()
+        elif select == "2":
+            browse_menu(db)
+            break
+        elif select == "3":
+            return quit()
 
-    def menu(self):
-        db = entry_db.EntryDB("entries.db")
-        print(greeting(db))
-        while True:
-            print("1. Make Entry\n2. Browse\n3. Quit")
-            select = input()
-            if select == "1":
-                input_text = multi_input("How are you feeling today?")
-                entry = journal_entry.JournalEntry(input_text)
-                db.add_entry(entry)
-                db.flush_to_disk()
-            elif select == "2":
-                browse_menu(db)
-                break
-            elif select == "3":
-                return quit()
+def browse_menu(self,db):
+    while True:
+        print("What entries would you like to view?")
+        browse_select = input("1. Browse all\n2. Text Search \n3. Date Search\n4. Back")
+        if browse_select == "1":
+            display_entries(db.get_all_entries())
+        elif browse_select == "2":
+            display_entries(search(db))
+        elif browse_select == "3":
+            display_entries(search_date(db))
+        elif browse_select == "4":
+            menu()
 
-    def browse_menu(self,db):
-        while True:
-            print("What entries would you like to view?")
-            browse_select = input("1. Browse all\n2. Text Search \n3. Date Search\n4. Back")
-            if browse_select == "1":
-                display_entries(db.get_all_entries())
-            elif browse_select == "2":
-                display_entries(search(db))
-            elif browse_select == "3":
-                display_entries(search_date(db))
-            elif browse_select == "4":
-                menu()
+def display_entries(self,entries):
+    for entry in entries:
+        if entry.creation_date().day == arrow.now().day:
+            print(f"{entry.creation_date().humanize()} {entry.content()}")
+        else:
+            print(f"{entry.creation_date().format('YYYY-MM-DD HH:mm:ss')} {entry.content()}")
 
-    def display_entries(self,entries):
-        for entry in entries:
-            if entry.creation_date().day == arrow.now().day:
-                print(f"{entry.creation_date().humanize()} {entry.content()}")
-            else:
-                print(f"{entry.creation_date().format('YYYY-MM-DD HH:mm:ss')} {entry.content()}")
+def search_date(self,db):
+    usr_date = input("Enter day (MM-DD-YY):")
+    usr_date = arrow.get(usr_date, 'MM-DD-YY')
+    return db.search_date(usr_date.floor('day'),usr_date.ceil('day'))
 
-    def search_date(self,db):
-        usr_date = input("Enter day (MM-DD-YY):")
-        usr_date = arrow.get(usr_date, 'MM-DD-YY')
-        return db.search_date(usr_date.floor('day'),usr_date.ceil('day'))
+def search(self,db):
+    term = input("Search:")
+    return db.search(term)
 
-    def search(self,db):
-        term = input("Search:")
-        return db.search(term)
-
-    def multi_input(self,prompt=None):
-        #custom input function for multi-line entries
-        if prompt:
-            print(prompt)
-        content = []
-        while True:
-            try:
-                line = input()
-                content.append(line)
-            except EOFError:
-                return "/n".join(content)
+def multi_input(self,prompt=None):
+    #custom input function for multi-line entries
+    if prompt:
+        print(prompt)
+    content = []
+    while True:
+        try:
+            line = input()
+            content.append(line)
+        except EOFError:
+            return "/n".join(content)
 
 class SaveHandler():
     def __init__(self,entry,editor):
--- menu.py
+++ menu.py
@@ -1,4 +1,7 @@
 import urwid
+from entry_db import EntryDB
+from edit import EditDisplay
+from io import StringIO
 
 class MenuButton(urwid.Button):
     def __init__(self, caption, callback):
@@ -22,47 +25,17 @@ class SubMenu(urwid.WidgetWrap):
         top.open_box(self.menu)
 
 class Choice(urwid.WidgetWrap):
-    def __init__(self, caption):
+    def __init__(self, caption,handler):
         super(Choice, self).__init__(
             MenuButton(caption, self.item_chosen))
         self.caption = caption
+        self.handler = handler
 
     def item_chosen(self, button):
-        response = urwid.Text([u'  You chose ', self.caption, u'\n'])
-        done = MenuButton(u'Ok', exit_program)
-        response_box = urwid.Filler(urwid.Pile([response, done]))
-        top.open_box(urwid.AttrMap(response_box, 'options'))
+        self.handler()
 
 def exit_program(key):
     raise urwid.ExitMainLoop()
-
-menu_top = SubMenu(u'Pyjournal', [
-    SubMenu(u'Entry', [
-        Choice(u'Make Entry'),
-        Choice(u'Edit Entry'),
-    ]),
-    SubMenu(u'Views', [
-        SubMenu(u'Search Options', [
-            Choice(u'Search Date'),
-            Choice(u'Search "Content"'),
-        ]),
-        Choice(u'Browse All'),
-    ]),
-])
-
-palette = [
-    (None,  'light gray', 'black'),
-    ('heading', 'black', 'light gray'),
-    ('line', 'black', 'light gray'),
-    ('options', 'dark gray', 'black'),
-    ('focus heading', 'white', 'light blue'),#head choice colors
-    ('focus line', 'black', 'light blue'),#head choice colors
-    ('focus options', 'black', 'light gray'),
-    ('selected', 'white', 'dark blue')]
-focus_map = {
-    'heading': 'focus heading',
-    'options': 'focus options',
-    'line': 'focus line'}
 
 class HorizontalBoxes(urwid.Columns):
     def __init__(self):
@@ -75,6 +48,75 @@ class HorizontalBoxes(urwid.Columns):
             self.options('given', 24)))
         self.focus_position = len(self.contents) - 1
 
-top = HorizontalBoxes()
-top.open_box(menu_top.menu)
-urwid.MainLoop(urwid.Filler(top, 'middle', 10), palette).run()
+class SaveHandler():
+    def __init__(self,entry,editor):
+        self.entry = entry
+        self.editor = editor
+    def unhandled_keypress(self,k):
+        if k == "f5":
+            self.entry.set_content(self.editor.get_text())
+        else:
+            self.editor.unhandled_keypress(k)
+
+class JournalApp:
+    palette = [
+        (None,  'light gray', 'black'),
+        ('heading', 'black', 'light gray'),
+        ('line', 'black', 'light gray'),
+        ('options', 'dark gray', 'black'),
+        ('focus heading', 'white', 'light blue'),#head choice colors
+        ('focus line', 'black', 'light blue'),#head choice colors
+        ('focus options', 'black', 'light gray'),
+        ('selected', 'white', 'dark blue')]
+    focus_map = {
+        'heading': 'focus heading',
+        'options': 'focus options',
+        'line': 'focus line'}
+
+    def __init__(self):
+        self.box_menus = HorizontalBoxes()
+        self.box_menus.open_box(menu_top.menu)
+        self.db = EntryDB("entries.db")
+        self.loop = urwid.MainLoop(urwid.Filler(top, 'middle', 10), palette)
+    
+    def run(self):
+        self.loop.start()
+    
+    def edit_new(self):
+        self.loop.stop()
+        editor = EditDisplay(StringIO(""))
+        handler = SaveHandler(None,editor)
+        loop = urwid.MainLoop(editor.view, editor.palette,
+            unhandled_input=handler.unhandled_keypress)
+        loop.run()
+
+def main2():
+    app = JournalApp()
+    menu_top = SubMenu(u'Pyjournal', [
+        SubMenu(u'Entry', [
+            Choice(u'Make Entry',app.edit_new),
+            Choice(u'Edit Entry',None),
+        ]),
+        SubMenu(u'Views', [
+            SubMenu(u'Search Options', [
+                Choice(u'Search Date',None),
+                Choice(u'Search "Content"',None),
+            ]),
+            Choice(u'Browse All',None),
+        ]),
+    ])
+
+def main():
+    top.open_box(menu_top.menu)
+    urwid.MainLoop(urwid.Filler(top, 'middle', 10), palette).run()
+    db = EntryDB("entries.db")
+    entry = db.get_all_entries()[0]
+    editor = EditDisplay(StringIO(entry.content()))
+    handler = SaveHandler(entry,editor)
+    loop = urwid.MainLoop(editor.view, editor.palette,
+        unhandled_input=handler.unhandled_keypress)
+    loop.run()
+    db.flush_to_disk()
+
+if __name__ == "__main__":
+    main()
